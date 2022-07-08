4b5997d5273dd642e2015f66698a7320043314a7
Higor Moroni <higor.ferreira@melhorenvio.com>
2022-07-08 15:31:51 -0300

    feat: eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+export default {
+  "root": true,
+  "env": {
+    "node": true
+  },
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
+  ],
+  "parserOptions": {
+    "parser": "@babel/eslint-parser"
+  },
+  "rules": {
+    "no-unused-vars":
+      process.env.NODE_ENV === "production"
+        ? "error"
+        : "warn"
+  }
+};
